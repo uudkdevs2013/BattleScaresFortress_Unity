4d4d2815180aeb48bce34397038f4f0ffe5dffb4
schimdt.cpp <schmidt.cpp@gmail.com>
2013-04-21 13:58:01 -0500

    I still don't know

---
+++ BattleScaresFortress/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using System.Collections;
+
+public class MainMenuController : MonoBehaviour
+{
+	
+	private void OnGUI()
+	{
+		GUI.Label(new Rect(20, 20, 200, 30), "Battle Scares Fortress");
+	}
+	
+}
